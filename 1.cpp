bf742bc86003d02ed8d877a6d488865fd8846dcf
Khanh <namkhanh153568@gmail.com>
2025-01-19 14:11:36 +0700

    feat: init project

---
+++ 1.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main() {
+	int a = 70;
+	int b = 30;
+	printf("a + b = %d", a + b);
+	return 0;
+}
---
+++ Untitled-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!");
+    return 0;
+    }
+
+    void printSubtraction(int a, int b) {
+        printf("The result of %d - %d is %d\n", a, b, a - b);
+    }
+
+    int main() {
+        printf("Hello World!\n");
+        printSubtraction(10, 5);
+        return 0;
+}
